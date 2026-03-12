4678408df6a6a8e8eae49d675b10e41a0c649bae
MohRamdani28 <zkram48@gmail.com>
2026-03-12 16:13:04 +0700

    update ke2

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,223 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    String title = 'Aplikasi Pertamaku';
-    String name = 'Moh Ramdani';
-    String message = 'Halo Nama Saya $name, Selamat datang di $title';
-
+class HomePage extends StatelessWidget {
+  build(context){
     return MaterialApp(
-      title: title,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title : Text(title),
+          backgroundColor: const Color.fromARGB(255, 237, 105, 4),
+          leading: Icon(Icons.home, color: const Color.fromARGB(255, 216, 215, 212,), size: 35,),
+          title: Text('JAJANAN GARUT',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: .bold,
+            color: const Color.fromARGB(255, 231, 230, 227)
+          ),
+          ),
         ),
-        body: Column(
-          children: [
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Padding(padding: EdgeInsets.all(8.0),
-            child: Text(message))
-            ],
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.archive),
+                    Text("New Artikel", style: TextStyle(fontWeight: .bold)),
+                  ],
+                ),
+                Card(
+                  child: Column(
+                    crossAxisAlignment: .start,
+                    children: [
+                     Image.network("https://1.bp.blogspot.com/-5OAf2IoncRE/X7N2H6WxErI/AAAAAAAABl4/BpMIKH9qKdgBbgVdZAAXM-3E2b5OBu-XgCLcBGAsYHQ/s900/Ayam-Geprek-1.jpg"),
+                     Text("Ayam Geprek Juara: Sensasi Pedas Gurih yang Bikin Ketagihan",
+                     style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: .bold
+                      ),
+                     ),
+                     Text("Ayam geprek juara menjadi salah satu kuliner favorit pecinta pedas. Perpaduan ayam goreng crispy yang digeprek bersama sambal segar menghasilkan rasa gurih, pedas, dan nikmat yang sulit dilupakan. Menu ini cocok dinikmati kapan saja, baik saat makan siang maupun makan malam."),
+                    ],
+                  ),
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    children: [
+                      Icon(Icons.message),
+                      Text("komentar",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: .bold
+                      ),
+                      ),
+                    ],
+                  ),
+                  ListView(
+                    shrinkWrap: true,
+                    children: [
+                      Card (
+                        child: Padding(
+                          padding:EdgeInsets.all(3),
+                          child: Column(
+                          crossAxisAlignment: .start,
+                          children: [
+                            Text("Zaki",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: .bold
+                            ),
+                            ),
+                            Text("Lalapan dan sambalnya segar, bikin rasa ayam gepreknya makin nikmat."),
+                          ],)
+          
+                        ),
+                      ),
+                      Card (
+                        child: Padding(
+                          padding:EdgeInsets.all(3),
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                            Text("Agus",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: .bold
+                            ),
+                            ),
+                            Text("Ladaa mennnn"),
+                          ],)
+          
+                        ), 
+                      ),
+                      Card (
+                        child: Padding(
+                          padding:EdgeInsets.all(3),
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                            Text("Acil",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: .bold
+                            ),
+                            ),
+                            Text("Cocok buat makan siang atau malam, rasanya selalu konsisten enak"),
+                          ],)
+          
+                        ),
+                      ),
+                      Card (
+                        child: Padding(
+                          padding:EdgeInsets.all(3),
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                            Text("Faiz",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: .bold
+                            ),
+                            ),
+                            Text("Cabenya nyabe bangett"),
+                          ],)
+          
+                        ),
+                      ),
+                      Card (
+                        child: Padding(
+                          padding:EdgeInsets.all(3),
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                            Text("Asan aja",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: .bold
+                            ),
+                            ),
+                            Text("Enakk bangettt guroihh"),
+                          ],)
+          
+                        ),
+                      ),
+                      Card (
+                        child: Padding(
+                          padding:EdgeInsets.all(3),
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                            Text("Raflie anak bontot",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: .bold
+                            ),
+                            ),
+                            Text("Harganya terjangkau tapi rasanya seperti makanan restoran mahal."),
+                          ],)
+          
+                        ),
+                      ),
+                      Card (
+                        child: Padding(
+                          padding:EdgeInsets.all(3),
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                            Text("alya cipaling bjw",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: .bold
+                            ),
+                            ),
+                            Text("Ayamnya gurih dan lembut, cocok banget dimakan sama nasi hangat."),
+                          ],)
+          
+                        ),
+                      ),
+                      Card (
+                        child: Padding(
+                          padding:EdgeInsets.all(3),
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                            Text("agus buntung",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: .bold
+                            ),
+                            ),
+                            Text("Rasanya juara, pantes banyak yang bilang ini ayam geprek terbaik."),
+                          ],)
+          
+                        ),
+                      ),
+                      Card (
+                        child: Padding(
+                          padding:EdgeInsets.all(3),
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                            Text("Suci na raja",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: .bold
+                            ),
+                            ),
+                            Text("Ayam gepreknya enak banget, ayamnya crispy dan sambalnya pedasnya pas!"),
+                          ],)
+          
+                        ),
+                      ),
+                    ],
+                  )
+              ],
+            )
+          ),
         ),
       ),
     );
